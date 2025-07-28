94e49752a63dc291084a53129c96dfcd64249727
Kürşat Abaylı <krst.abayli@outlook.com>
2025-07-28 04:44:35 +0300

    feat(vault): Implement add and list functionality for passwords

---
+++ src/clients/SecureVault.App.Services/Models/VaultItemModels/PasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SecureVault.App.Services.Models.VaultItemModels
+{
+    public class PasswordViewModel
+    {
+        public PasswordModel Model { get; init; } = new();
+        public bool IsPasswordVisible { get; set; } = false;
+        public string DisplayPassword => IsPasswordVisible ? Model.Password : "••••••••";
+    }
+}
